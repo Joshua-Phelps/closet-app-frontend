648ed1ad034db49e5d58bf1d8a9b893d423c6076
jugtuttle <jugtuttle@gmail.com>
2020-01-22 10:58:20 -0800

    item index items on page

--- src/index.js
+++ src/index.js
@@ -6,12 +6,44 @@ document.addEventListener("DOMContentLoaded", () => {
     fetchItems()
 })
 
+
+
 const fetchItems = () => {
     fetch("http://localhost:3000/items")
     .then(resp => resp.json())
-    .then(json => console.log(json))
+    .then(json => seperateItems(json))
     // .then(json => itemsArray(json))
 }
+
+const seperateItems = items => {
+    items.forEach(item => appendItem(item) )
+}
+
+
+const appendItem = item => {
+    const showDiv = document.getElementById("show-item")
+    const itemDiv = makeItemCard(item)
+    showDiv.appendChild(itemDiv)
+}
+
+const makeItemCard = item => {
+    console.log(item)
+    const itemDiv = document.createElement("div")
+    itemDiv.className = "card" 
+
+    const img = document.createElement("img")
+    img.src = item.image_url 
+    
+    const h3 = document.createElement("h3")
+    h3.innerText = item["name"]
+
+    itemDiv.appendChild(h3)
+    itemDiv.appendChild(img)
+
+    return itemDiv
+}
+
+
 
 // const itemsArray = (itemsArray) => {
 //     itemsArray.forEach(item => console.log(item))
