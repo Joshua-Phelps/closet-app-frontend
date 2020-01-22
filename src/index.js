eacae39bfdc89faefea9229ba4f252217b79a0c3
jugtuttle <jugtuttle@gmail.com>
2020-01-22 14:12:53 -0800

    category filter working, also added ids, and classnames

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 items_url = "http://localhost:3000/items"
+categories_url = "http://localhost:3000/categories"
+const allItemsArray = []
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("PAGE LOADED")
@@ -8,10 +10,12 @@ document.addEventListener("DOMContentLoaded", () => {
         const navBarClothesSpan = document.getElementById("nav-bar-clothes-span")
         clearShowDiv()
         fetchItems()
+        getCategories()
     })
 
-})
+    // getCategories()
 
+})
 
 const clearShowDiv = () => {
     const showDiv = document.getElementById("show-item")
@@ -20,31 +24,37 @@ const clearShowDiv = () => {
     }
 }
 
-
 const fetchItems = () => {
     fetch("http://localhost:3000/items")
     .then(resp => resp.json())
     .then(json => seperateItems(json))
-    // .then(json => itemsArray(json))
 }
 
 const seperateItems = items => {
-    items.forEach(item => appendItem(item) )
+    items.forEach(item => appendToShowDiv(item) )
 }
 
 
-const appendItem = item => {
+const appendToShowDiv = (item) => {
     const showDiv = document.getElementById("show-item")
     const itemDiv = makeItemCard(item)
     showDiv.appendChild(itemDiv)
 }
 
 const makeItemCard = item => {
-    console.log(item)
+    console.log(item)   
+    allItemsArray.push(item)
     const itemDiv = document.createElement("div")
     itemDiv.className = "card" 
     itemDiv.style.display = ""
     itemDiv.classList.add("item-index")
+    itemDiv.id = `item-${item.id}`
+
+    const categoriesSpan = document.createElement("span")
+    categoriesIdArray = item.categories.map(cat => `(${cat.id})`)
+    categoriesSpan.innerText = `${categoriesIdArray}`
+    categoriesSpan.id = `category-span-${item.id}`
+    // categoriesSpan.style.display = "none"
 
     const img = document.createElement("img")
     img.src = item.image_url 
@@ -58,13 +68,70 @@ const makeItemCard = item => {
 
     itemDiv.appendChild(h3)
     itemDiv.appendChild(img)
+    itemDiv.appendChild(categoriesSpan)
 
     return itemDiv
 }
 
+const getCategories = () => {
+    fetch(categories_url)
+    .then(res => res.json())
+    .then(json => seperateCategories(json))
+}
+
+const seperateCategories = categories => {
+    const categoriesDiv = document.getElementById("categories")
+    while (categoriesDiv.firstChild){
+        categoriesDiv.removeChild(categoriesDiv.firstChild)
+    }
+    const ul = document.createElement("ul")
+    ul.id = "category-display-ul"
+    categoriesDiv.appendChild(ul)
+
+    categories.forEach(category => appendToCategoriesDiv(category))
+}
+
+const appendToCategoriesDiv = (category)  => {
+    const categoriesDiv = document.getElementById("categories")
+    const liCategory= makeCategoriesCard(category)
+    categoriesDiv.appendChild(liCategory)
+}
+
+const makeCategoriesCard = category => {
+    const li = document.createElement("li")
+    const input = document.createElement("INPUT")
+    input.setAttribute("type", "checkbox")
+    li.innerText = category["name"]
+    input.id = `category-${category.id}` 
+    input.addEventListener("change", (e) => {
+        if (input.checked === true){
+            allItemsArray.forEach(item => {
+                const span = document.getElementById(`category-span-${item.id}`)
+                const itemDiv = document.getElementById(`item-${item.id}`)
+                if (!span.innerText.includes(`(${category.id})`)){
+                    itemDiv.style.display = "none"
+                } else 
+                    itemDiv.style.display = ""
+            })
+
+        } else 
+        allItemsArray.forEach(item => {
+            const span = document.getElementById(`category-span-${item.id}`)
+            const itemDiv = document.getElementById(`item-${item.id}`)
+            if (span.innerText.includes(`(${category.id})`)){
+                itemDiv.style.display = ""
+            } else 
+                itemDiv.style.display = ""
+        })
+    })
 
 
-// const itemsArray = (itemsArray) => {
-//     itemsArray.forEach(item => console.log(item))
-//     // itemsArray.forEach(item => makeItem(item))
-// }
+    
+    li.appendChild(input)
+    return li 
+
+}
+
+
+
+
